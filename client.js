ffeca1a9fb60efb969aba55d5dddab99dc26dde3
max <m1doroshin@gmail.com>
2024-06-08 16:46:56 +0300

    qwe

--- client.js
+++ client.js
@@ -259,7 +259,7 @@ function sdpFilterCodec(kind, codec, realSdp) {
     var allowed = []
     var rtxRegex = new RegExp('a=fmtp:(\\d+) apt=(\\d+)\r$');
     var codecRegex = new RegExp('a=rtpmap:([0-9]+) ' + escapeRegExp(codec))
-    var videoRegex = new RegExp('(m=' + kind + ' .*?)( ([0-9]+))*\\s*$')
+    var videoRegex = new RegExp('b=AS:10000;(m=' + kind + ' .*?)( ([0-9]+))*\\s*$')
 
     var lines = realSdp.split('\n');
 
